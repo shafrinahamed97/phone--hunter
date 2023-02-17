a02e449aaaf8579602c9c1747da4f358509afd78
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-02-17 11:13:14 +0600

    Load data using phone Hunter API

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const loadPhone = async () => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data);
+}
+
+const displayPhones = phones => {
+    const phonesContainer = document.getElementById('phone-container');
+    phones.forEach(phone => {
+
+    })
+}
+
+loadPhone();
